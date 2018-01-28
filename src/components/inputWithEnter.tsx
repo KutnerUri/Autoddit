273640f333b0f9c8d0eeb4ab5bab46ccf34218c9
Uri <Urikutner@gmail.com>
2018-01-28 23:00:42 +0200

    extract input component so we could type enter for our username

---
+++ src/components/inputWithEnter.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+
+export interface Props {
+	onSubmission: (val: string) => void;
+}
+
+export default class InputWithEnter extends React.Component<Props, { val: string }> {
+	constructor(props: Props) {
+		super(props);
+		this.state = { val: '' };
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleKeyPress = this.handleKeyPress.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event: React.FormEvent<HTMLInputElement>) {
+		this.setState({ val: event.currentTarget.value });
+	}
+
+	handleKeyPress(event: React.KeyboardEvent<HTMLInputElement>) {
+		if (event.key !== 'Enter') { return; }
+
+		this.handleSubmit();
+	}
+
+	handleSubmit() {
+		this.props.onSubmission(this.state.val);
+	}
+
+	render() {
+		return (
+			<span>
+				<input value={this.state.val} onChange={this.handleChange} onKeyPress={this.handleKeyPress} />
+				<button onClick={this.handleSubmit}>{this.props.children}</button>
+			</span>
+		);
+	}
+}
--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { connect, Dispatch } from 'react-redux';
 import * as actions from '../actions/index';
+import InputWithEnter from './inputWithEnter';
 
 interface Props {
 	login?: (username: string) => void;
@@ -10,18 +11,11 @@ class Login extends React.Component<Props, { username: string }> {
 	constructor(props: Props) {
 		super(props);
 
-		this.state = { username: '' };
-
-		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmission = this.handleSubmission.bind(this);
 	}
 
-	handleChange(event: React.FormEvent<HTMLInputElement>) {
-		this.setState({username: event.currentTarget.value});
-	}
-
-	handleSubmission() {
-		this.props.login(this.state.username);
+	handleSubmission(val: string) {
+		this.props.login(val);
 	}
 
 	render() {
@@ -29,8 +23,9 @@ class Login extends React.Component<Props, { username: string }> {
 			<div>
 				<h2>Please login:</h2>
 				<span>username:</span>
-				<input value={this.state.username} onChange={this.handleChange} />
-				<button onClick={this.handleSubmission}>submit</button>
+				<InputWithEnter onSubmission={this.handleSubmission}>
+					Submit
+				</InputWithEnter>
 			</div>
 		);
 	}
